ac6fae1a849bbd6c17a2f9339acf548476839680
Arwel Brown <arwel.brown@hotmail.co.uk>
2024-04-03 20:14:19 +0100

    updated to dotfiles 2.8.4

--- waybar/themeswitcher.sh
+++ waybar/themeswitcher.sh
@@ -18,10 +18,12 @@ themes_path="$HOME/dotfiles/waybar/themes"
 # ----------------------------------------------------- 
 listThemes=""
 listNames=""
+listNames2=""
 
 # ----------------------------------------------------- 
 # Read theme folder
-# ----------------------------------------------------- 
+# -----------------------------------------------------
+sleep 0.2 
 options=$(find $themes_path -maxdepth 2 -type d)
 for value in $options
 do
@@ -34,8 +36,10 @@ do
                 if [ -f $themes_path$result/config.sh ]; then
                     source $themes_path$result/config.sh
                     listNames+="$theme_name\n"
+                    listNames2+="$theme_name~"
                 else
                     listNames+="/${arrThemes[1]};$result\n"
+                    listNames2+="/${arrThemes[1]};$result~"
                 fi
             fi
         fi
@@ -46,7 +50,10 @@ done
 # Show rofi dialog
 # ----------------------------------------------------- 
 listNames=${listNames::-2}
-choice=$(echo -e "$listNames" | rofi -dmenu -i -replace -config ~/dotfiles/rofi/config-themes.rasi -no-show-icons -width 30 -p "Themes" -format i) 
+choice=$(echo -e "$listNames" | rofi -dmenu -replace -i -config ~/dotfiles/rofi/config-themes.rasi -no-show-icons -width 30 -p "Themes" -format i)
+IFS="~"
+input=$listNames2
+read -ra array <<< "$input"
 
 # ----------------------------------------------------- 
 # Set new theme by writing the theme information to ~/.cache/.themestyle.sh
@@ -55,4 +62,5 @@ if [ "$choice" ]; then
     echo "Loading waybar theme..."
     echo "${listThemes[$choice+1]}" > ~/.cache/.themestyle.sh
     ~/dotfiles/waybar/launch.sh
+    notify-send "Waybar Theme changed" "to ${array[$choice]}"
 fi
